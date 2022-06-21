fad0905450b371c13b2a2f3d3c968b597131fa51
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2022-06-21 22:34:30 +0500

    notification test

---
+++ test/src/view/notification_test.dart
@@ -0,0 +1,27 @@
+import 'package:chucker_flutter/src/view/helper/chucker_ui_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'Notification should appear when showNotification called ',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          navigatorObservers: [ChuckerFlutter.navigatorObserver],
+          home: Scaffold(appBar: AppBar()),
+        ),
+      );
+
+      ChuckerUiHelper.showNotification(
+        method: 'GET',
+        statusCode: 200,
+        path: 'path',
+      );
+
+      await tester.pumpAndSettle(const Duration(seconds: 3));
+
+      expect(ChuckerUiHelper.notificationShown, true);
+    },
+  );
+}
